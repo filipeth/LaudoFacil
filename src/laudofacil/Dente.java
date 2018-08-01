93b57e5e243d5893dfb8c761639a9caf464530f2
Filipe Theodoro <filipeth@globo.com>
2018-08-01 13:18:21 -0400

    Criacao do objeto dente

--- src/laudofacil/Dente.java
+++ src/laudofacil/Dente.java
@@ -5,11 +5,20 @@ package laudofacil;
 public class Dente {
     private boolean carie;
     private boolean presente;
-    private boolean carie;
-    private boolean carie;
-    private boolean carie;
-    private boolean carie;
-    private boolean carie;
+    private boolean implante;
+    private boolean raizResidual;
+    private boolean tratamentoEndodôntico;
+    private boolean núcleoMetálicoIntraCanal;
+    private boolean lesãoOsseaPeriapical;
+    private boolean mésioAngulados;
+    private boolean giroversão;
+    private boolean dilaceraçãoRadicular;
+    private boolean apinhamento;
+    private boolean extrusão;
+    private boolean remodelamentoApical;
+    private boolean escleroseOssea;
+    private boolean presençaCálculo;
+    
     
     
 }
