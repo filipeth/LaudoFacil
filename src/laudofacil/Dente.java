b16e6bb09f6774457a34f003a41c17d37a5bf6fd
Filipe Theodoro <filipeth@globo.com>
2018-08-01 13:00:55 -0400

    Criacao do objeto dente

---
+++ src/laudofacil/Dente.java
@@ -0,0 +1,15 @@
+
+package laudofacil;
+
+
+public class Dente {
+    private boolean carie;
+    private boolean presente;
+    private boolean carie;
+    private boolean carie;
+    private boolean carie;
+    private boolean carie;
+    private boolean carie;
+    
+    
+}
