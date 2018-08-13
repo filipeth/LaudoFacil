3d9033fd0d210e8001d3102aefbe094fc276a5dd
Filipe Theodoro <filipeth@globo.com>
2018-08-13 19:34:14 -0400

    Criacao do Main

--- src/laudofacil/Main.java
+++ src/laudofacil/Main.java
@@ -15,13 +15,17 @@ public class Main {
     public static void main(String args[]) {
         NewJFrame nf = new NewJFrame();
         nf.setVisible(true);
-        ArrayList<Dente> listaDentes = new ArrayList<>();
-        for(int k =0; k<=3; k++){
-            for(int i = 11; i < 19; i++){
-                listaDentes.add(new Dente(i+10*k));
-                System.out.println(i+10*k);
-            }
-        }
+//        ArrayList<Dente> listaDentes = new ArrayList<>();
+//        for(int k =0; k<=3; k++){
+//            for(int i = 11; i < 19; i++){
+//                listaDentes.add(new Dente(i+10*k));
+//                System.out.println(i+10*k);
+//            }
+//        }
+//        os 8 primeiros sao os dentes 1x
+//        depois os dentes 2x, 3x, 4x.
+        
+        
         
     }
 }
