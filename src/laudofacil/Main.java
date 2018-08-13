f9e450384b23bad09d16de41acf7243d11c62429
Filipe Theodoro <filipeth@globo.com>
2018-08-13 14:31:58 -0400

    Criacao do Main

---
+++ src/laudofacil/Main.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package laudofacil;
+
+/**
+ *
+ * @author filipetheodoro
+ */
+public class Main {
+    public static void main(String args[]) {
+        NewJFrame nf = new NewJFrame();
+        nf.setVisible(true);
+    }
+}
